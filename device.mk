07c4642f5fa7f13e535f28cd19821c4b75bdcbb8
Akash <akashniki@gmail.com>
2021-11-05 19:30:47 +0100

    cmi: build hidl based on CAF's requirement

--- device.mk
+++ device.mk
@@ -166,6 +166,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
