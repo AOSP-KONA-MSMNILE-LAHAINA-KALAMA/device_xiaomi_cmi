5450bac0e93dbf72c0f3dd8b9f18212a2ba8030c
Akash Srivastava <akashniki@gmail.com>
2022-09-01 02:06:14 +0700

    Revert "cmi: Switch to AIDL WIFI HAL"
    This reverts commit 0ef87a73893a44b25c94b7c1413cd638c2d2ea91.

--- device.mk
+++ device.mk
@@ -258,11 +258,12 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    android.hardware.wifi.supplicant@1.4.vendor \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
     libwpa_client \
+    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.2.vendor \
     wpa_supplicant \
     wpa_supplicant.conf
 
