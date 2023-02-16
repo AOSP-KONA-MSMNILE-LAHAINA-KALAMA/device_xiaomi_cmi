28c0b908583ff17d4c9425416379a2d3047dd038
Jake Weinstein <jake@aospa.co>
2023-02-17 02:27:00 +0700

    cmi: rootdir: Do not allow restricted tasks to run on big cores
    The restricted cpuset is for system tasks that are
    throttled because the screen is off. Google only
    runs these tasks on the little cluster
    to save power and we will follow suit.
    
    Change-Id: I0174beb11dae2109df4ddf65304bd50c5cc05264

--- init/init.qcom.post_boot.sh
+++ init/init.qcom.post_boot.sh
@@ -186,6 +186,7 @@ case "$target" in
 	# cpuset parameters
         echo 1-2     > /dev/cpuset/audio-app/cpus
 	echo 0-1     > /dev/cpuset/background/cpus
+	echo 0-3     > /dev/cpuset/restricted/cpus
 	echo 0-3     > /dev/cpuset/system-background/cpus
 	echo 4-6     > /dev/cpuset/foreground/boost/cpus
 	echo 0-6     > /dev/cpuset/foreground/cpus
